b11de431499c4a68a4a7b0c20e3bcae2cc1760b9
thesuryac <156649224+thesuryac@users.noreply.github.com>
2024-07-02 13:42:01 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       "http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=10000",
       {
         headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE5OTA0NTUzLCJpYXQiOjE3MTk5MDQyNTMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImQxMWU3MjBiLWJiNGEtNDhmMi1iNTY5LTk2OWZmOGVkMzM4MCIsInN1YiI6InN1cnlhY3AuY29tQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6Ik1haGVuZHJhIGNvbGxlZ2Ugb2YgZW5naW5lZXJpbmciLCJjbGllbnRJRCI6ImQxMWU3MjBiLWJiNGEtNDhmMi1iNTY5LTk2OWZmOGVkMzM4MCIsImNsaWVudFNlY3JldCI6InBMSFZkWVdNeXRURmFQd2QiLCJvd25lck5hbWUiOiJTdXJ5YSBDIiwib3duZXJFbWFpbCI6InN1cnlhY3AuY29tQGdtYWlsLmNvbSIsInJvbGxObyI6IjYyMTUyMTEwNDEzOSJ9.DUCfsL1WGH3PXPAgIK56-TFu2MD2_hujNxz4RoLzES4`,
+          Authorization: `Bearer`,
         },
       }
     )
